79d483eaa5d09877d0630927bddcf5792096acdc
Azure <961523404@qq.com>
2018-01-19 21:29:18 +0800

    添加百度OCR

---
+++ MillionHerosHelper/BaiDuOCR.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using Baidu.Aip.Ocr;
+
+namespace MillionHerosHelper
+{
+    public static class BaiDuOCR
+    {
+        private const string API_KEY = "xxx";
+        private const string SECRET_KEY = "xxx";
+        private static Ocr OCR;
+
+        public static void InitBaiDuOCR(string api_key, string secret_key)
+        {
+            OCR = new Ocr(api_key, secret_key);
+        }
+        public static string get(byte[] image)
+        {
+            if (OCR == null)
+            {
+                InitBaiDuOCR(API_KEY, SECRET_KEY);
+            }
+
+            JObject res = OCR.GeneralBasic(image);
+
+            JToken[] arr = res["words_result"].ToArray();
+            StringBuilder sb = new StringBuilder();
+            foreach (JToken item in arr)
+            {
+                sb.AppendLine(item["words"].ToString());
+            }
+            return sb.ToString();
+        }
+
+    }
+}
