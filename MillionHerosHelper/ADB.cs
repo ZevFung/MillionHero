57f37daffdd389b685d0cd60dee18304314f4340
Azure <961523404@qq.com>
2018-01-19 21:29:37 +0800

    封装少量ADB命令

---
+++ MillionHerosHelper/ADB.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace MillionHerosHelper
+{
+    public static class ADB
+    {
+        public static bool CheckConnect()
+        {
+            string res = RunADBCommand("adb devices");
+            try
+            {
+                if (res.IndexOf("device", 25) == -1)
+                    return false;
+                else
+                    return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        /// <summary>
+        /// 获取屏幕截图,请确保手机已经连接
+        /// </summary>
+        /// <returns>返回图片路径(临时)</returns>
+        public static byte[] GetScreenshort()
+        {
+            if (!Directory.Exists("screenshort"))
+            {
+                Directory.CreateDirectory("screenshort");
+            }
+
+            string folder = AppDomain.CurrentDomain.BaseDirectory + "screenshort\\";
+            string fileName = DateTime.Now.ToFileTime().ToString() + ".png";
+
+            RunADBCommand("adb shell screencap -p /sdcard/" + fileName);
+            RunADBCommand("adb pull /sdcard/" + fileName + " " + folder + fileName);
+            RunADBCommand("adb shell rm /sdcard/" + fileName);
+            System.Diagnostics.Debug.WriteLine(folder + fileName);
+            byte[] image = File.ReadAllBytes(folder + fileName);
+            File.Delete(folder + fileName);
+            return image;
+        }
+
+        public static string RunADBCommand(string command)
+        {
+            return ProcessHelper.RunProcessAndGetOutPut("cmd.exe", @"/c " + command, AppDomain.CurrentDomain.BaseDirectory + @"ADB");
+        }
+    }
+}
---
+++ MillionHerosHelper/Helper/ProcessHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace MillionHerosHelper
+{
+    class ProcessHelper
+    {
+        public static string RunProcessAndGetOutPut(string fileName, string args, string workingDirectory = "")
+        {
+            try
+            {
+                Process run = new Process();
+                run.StartInfo.FileName = fileName;
+                run.StartInfo.Arguments = args;
+                if (String.IsNullOrEmpty(workingDirectory))//设置工作目录
+                {
+                    run.StartInfo.UseShellExecute = true;
+                }
+                else
+                {
+                    run.StartInfo.UseShellExecute = false;
+                    run.StartInfo.WorkingDirectory = workingDirectory;
+                }
+
+                run.StartInfo.RedirectStandardInput = true;
+                run.StartInfo.RedirectStandardOutput = true;
+                run.StartInfo.RedirectStandardError = true;
+                run.StartInfo.CreateNoWindow = true;
+
+                run.Start();
+                string res = run.StandardOutput.ReadToEnd();
+                run.WaitForExit();
+                run.Close();
+                return res;
+            }
+            catch
+            {
+                return "error";
+            }
+        }
+
+    }
+}
